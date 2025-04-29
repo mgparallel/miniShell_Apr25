0dbae7a51171bbc1f2ba427abd2fee509b580bdf
Gabryan8 <omfgabri@gmail.com>
2025-04-29 21:28:01 +0200

    echo

--- 00_Meg/echo.c
+++ 00_Meg/echo.c
@@ -1,14 +1,29 @@
 #include "minishell.h"
 
-// int	cmd_echo(char **arr)
-// {
-// 	char *ag;
-// 	int	i;
+int cmd_echo(int argc, char **argv)
+{
+    int i;
+     
+    i = 0;
+    if (strcmp(argv[0], "-n") == 0)
+        i++;
+    while (argv[i])
+    {
+        printf("%s", argv[i]);
+        if (i < argc - 1)
+            printf(" ");
+    }
+    if (strcmp(argv[0], "-n") == 0)
+        printf("\n");
+    return (0);
+}
 
-// 	if (!*arr)
-// 		return (1);
-// 	ag = *arr;
-// 	i = 1;
-// 	while (ag[i])
-
-// }
+int exec_echo(char **args)
+{
+    int argc;
+    
+    argc = 0;
+    while (args[argc] != NULL)
+        argc++;
+    return (cmd_echo(argc, args));
+}
