aa9367352b87f6cd48618a8dea3c57f6fe95d016
mgparallel <mgparallel.pm7@gmail.com>
2025-07-19 14:01:14 +0200

    fixed unset

--- 00_Meg/03_builtin/cmd_unset.c
+++ 00_Meg/03_builtin/cmd_unset.c
@@ -6,7 +6,7 @@
 /*   By: menwu <menwu@student.42barcelona.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 22:34:24 by menwu             #+#    #+#             */
-/*   Updated: 2025/07/16 20:22:01 by menwu            ###   ########.fr       */
+/*   Updated: 2025/07/19 14:00:54 by menwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 int	free_node(t_files **ref)
 {
-	free((*ref)->value);
+	if (!ref || !*ref)
+		return (0);
+	if ((*ref)->value)
+		free((*ref)->value);
 	free(*ref);
+	*ref = NULL;
 	return (0);
 }
 
@@ -34,11 +38,9 @@ int	cmd_unset(char *var, t_files **env)
 		{
 			temp = ref->next;
 			if (prev == NULL)
-			{
 				*env = temp;
-				return (0);
-			}
-			prev->next = temp;
+			else
+				prev->next = temp;
 			return (free_node(&ref));
 		}
 		prev = ref;
